a12c65535f8124b3f6ccba6f2617cf893e5d4fac
kblackwelder <codingblindtech@gmail.com>
2024-04-28 10:36:22 -0400

    Error handling implemented. Now implementing logger util in repo.

--- src/global_process_handler.py
+++ src/global_process_handler.py
@@ -1,14 +1,12 @@
 import concurrent.futures
 import logging
 import os
-import itertools
 
 # logger = logging.getLogger(f'global_process.{__name__}')
 
 
 class GlobalProcessHandler():
     def __init__(self, funcs_to_run, cpus_to_use=0):
-        # Can run one function, but allows for parallel processing
         self.funcs_to_run = funcs_to_run
         self.os_core_count = os.cpu_count()
 
@@ -17,22 +15,23 @@ class GlobalProcessHandler():
         else:
             self.max_workers = cpus_to_use
 
-        self.initializer = None
-        self.initargs = ()
-        self.max_tasks_per_child = None
-
-        print("Getting started with multiprocessing global handler")
 
     def submit_modules(self, data_set=[]):
-        print(
-            f"The cores for max workers is going to be {self.max_workers}")
-
         with concurrent.futures.ProcessPoolExecutor(max_workers=self.max_workers) as executor:
             requests_to_make = []
             for module in self.funcs_to_run:
                 for data in data_set:
-                    job_req = executor.submit(module, data)
+                    try:
+                        job_req = executor.submit(module, data)
+                    except Exception as e:
+                        print(f'Error submitting process: {e}')
+                        raise e
                     requests_to_make.append(job_req)
 
             for fn in concurrent.futures.as_completed(requests_to_make):
-                print(f"the result is {fn.result()}")
+                try:
+                    result = fn.result()
+                    print(f"the result is {result}")
+                except Exception as e:
+                    print(f'Error getting result: {e}')
+                    raise e
--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import sys
 from src.global_process_handler import GlobalProcessHandler
 from time import sleep, time
 
@@ -5,7 +6,7 @@ from time import sleep, time
 def generic_function_add(n):
     # sleep for 2 seconds
     sleep(2)
-    print(f'Returning {n + 4}')
+    print(f'This is the next number being added: {n}')
     return n + 4
 
 
@@ -13,7 +14,16 @@ def main():
     data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     process_handler = GlobalProcessHandler([generic_function_add])
     # Submit the data with job
-    process_handler.submit_modules(data)
+    try:
+        process_handler.submit_modules(data)
+    except Exception as e:
+        print(e)
+        sys.exit()
+
+
+    # Loop below shows example without process handler
+    # for i in data:
+    #     generic_function_add(i)
 
 
 if __name__ == '__main__':
